f38e090236202230f57da8d0ffbf0d6cf378ed11
Rich Vogt <rvogt@signal.co>
2017-01-29 17:09:47 -0600

    simple old school servlet that responds to doGet

---
+++ src/main/java/com/shamblingcoder/mypackage/ServletExample.java
@@ -0,0 +1,26 @@
+package com.shamblingcoder.mypackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletExample extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		out.println("<html><head><title>Servlet With Relish</title></head><body>");
+		out.println("<h1>Here we go!</h1></body></html>");
+		out.close();
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		super.doPost(request, response);
+	}
+}
